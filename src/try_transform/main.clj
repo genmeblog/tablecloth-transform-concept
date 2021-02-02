860c95e2471d9df2e1a5152ff79bbcd9694af67b
Carsten Behring <carsten.behring@gmail.com>
2021-02-02 22:16:59 +0100

    removed map-ds dualty, use pipeline-context only

--- src/try_transform/main.clj
+++ src/try_transform/main.clj
@@ -10,86 +10,64 @@
 
 
 
-;;;  ease the duality of map-dataset
-
-(defn ->ds [ds-or-map]
-  (get ds-or-map :dataset ds-or-map )
-
-  )
-
-(defn merge-or-assoc-ds
-  ([id ds-or-map new-map]
-   (if (satisfies? proto-ds/PColumnarDataset ds-or-map)
-     (assoc new-map
-            :dataset ds-or-map)
-     (assoc ds-or-map id new-map)
-     ))
-  ([ds-or-map]
-   (merge-or-assoc-ds ds-or-map {})
-   )
-
-  )
-
-(defn associate-ds [ds-or-map ds]
-  (if (satisfies? proto-ds/PColumnarDataset ds-or-map)
-    {:dataset ds}
-    (merge ds-or-map
-           {:dataset ds}
-           )
-    )
-  )
 
 
 ;;;  adapt exiting function to be transform compliant
-(defn drop-rows [ds-or-map rows-selector]
-  (associate-ds ds-or-map
-                (tc/drop-rows  (->ds ds-or-map) rows-selector))
+(defn drop-rows [pipeline-context rows-selector]
+  (assoc pipeline-context
+         :dataset
+         (tc/drop-rows  (:dataset pipeline-context) rows-selector))
   )
 
-(defn categorical->number [ds-or-map filter-fn-or-dataset]
-  (associate-ds ds-or-map
-   (ds/categorical->number (->ds ds-or-map) filter-fn-or-dataset))
+(defn categorical->number [pipeline-context filter-fn-or-dataset]
+  (assoc pipeline-context
+         :dataset
+         (ds/categorical->number (:dataset pipeline-context) filter-fn-or-dataset))
   )
 
-(defn set-inference-target [ds-or-map target-name-or-seq]
-  (associate-ds ds-or-map
-                (ds-mod/set-inference-target (->ds ds-or-map) target-name-or-seq)))
+(defn set-inference-target [pipeline-context target-name-or-seq]
+  (assoc pipeline-context
+         :dataset
+         (ds-mod/set-inference-target (:dataset pipeline-context) target-name-or-seq)))
 
-(defn train-or-predict [ds-or-map id options ]
-  ;;;  this function requires map
-  (let [ds (:dataset ds-or-map)
-        result (case (:mode ds-or-map)
+(defn train-or-predict [pipeline-context id options ]
+  (let [ds (:dataset pipeline-context)
+        result (case (:mode pipeline-context)
                  :fit (ml/train ds options)
-                 :transform (ml/predict ds (get ds-or-map id) )
+                 :transform (ml/predict ds (get pipeline-context id) )
                  )]
-    (merge-or-assoc-ds id ds-or-map result)))
+    (assoc  pipeline-context id result)))
 
 
+(comment
 
 
-
-(def ds (tc/dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv"))
+  (def ds (tc/dataset "https://raw.githubusercontent.com/techascent/tech.ml/master/test/data/iris.csv"))
 
 
 
 ;;;  just fo demo, train=test
-(def train-ds ds)
-(def test-ds ds)
+  (def train-ds ds)
+  (def test-ds ds)
 
 
-(defn run-pipeline [ds context]
-  (-> (merge context {:dataset ds})
-      (drop-rows 2) ;; example for arbitray tabclecoth transformation
-      (categorical->number cf/categorical)
-      (set-inference-target "species")
-      (train-or-predict :xgboost-hinge {:model-type :xgboost/binary-hinge-loss})
-      (train-or-predict :xgboost-class {:model-type :xgboost/classification})))
+  (defn run-pipeline [ds context]
+    (-> (merge context {:dataset ds})
+        (drop-rows 2) ;; example for arbitray tabclecoth transformation
+        (categorical->number cf/categorical)
+        (set-inference-target "species")
+        (train-or-predict :xgboost-hinge {:model-type :xgboost/binary-hinge-loss})
+        (train-or-predict :xgboost-class {:model-type :xgboost/classification})
+        ))
 
 
-;; fit thge pipeline (including train)
-(def fit-result
-  (run-pipeline train-ds {:mode :fit}))
+  ;; fit thge pipeline (including train)
+  (def fit-result
+    (run-pipeline train-ds {:mode :fit}))
 
 ;;;  transform (= predict ) on test
-(def transform-result
-  (run-pipeline test-ds (merge fit-result {:mode :transform})))
+  (def transform-result
+    (run-pipeline test-ds (merge fit-result {:mode :transform})))
+
+
+  )
